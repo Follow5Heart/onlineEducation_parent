1491642d1b463ef41f2230197715e9199a614c58
ztyCode <1793959949@qq.com>
2023-01-19 23:55:16 +0800

    添加自定义通用型异常，用于controller层异常处理固定格式写法

---
+++ common/service_base/src/main/java/com/zty/onlineedu/service/base/exceptions/GeneralException.java
@@ -0,0 +1,47 @@
+package com.zty.onlineedu.service.base.exceptions;
+
+import com.zty.onlineedu.common.base.result.ResultCodeEnum;
+import lombok.Data;
+
+/**
+ * @Author zty
+ * @Date 2023/1/19 19:33
+ * 自定义的通用异常，用于controller层的统一异常处理
+ * 定义好，需要在全局异常统一处理
+ */
+@Data
+public class GeneralException extends RuntimeException{
+
+    private Integer code;
+    private ResultCodeEnum resultCodeEnum;
+
+
+
+    public GeneralException(String message, Integer  code){
+        super(message);
+        this.code = code;
+
+    }
+
+
+    public GeneralException(ResultCodeEnum resultCodeEnum){
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+        this.resultCodeEnum=resultCodeEnum;
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralException{" +
+                "code=" + code +
+                "message=" + resultCodeEnum.getMessage() +
+                '}';
+    }
+
+
+
+
+
+
+
+}
