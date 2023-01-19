18ab79da66ec8a069cf030ff85bbe613dd4ab239
ztyCode <1793959949@qq.com>
2023-01-20 00:01:09 +0800

    在edu_file的controller层，优化异常处理的固定格式

--- service/service_file/src/main/java/com/zty/onlineedu/files/controller/FileDetailController.java
+++ service/service_file/src/main/java/com/zty/onlineedu/files/controller/FileDetailController.java
@@ -3,12 +3,18 @@ package com.zty.onlineedu.files.controller;
 import cn.xuyanwu.spring.file.storage.FileInfo;
 import cn.xuyanwu.spring.file.storage.FileStorageService;
 import com.zty.onlineedu.common.base.result.Result;
+import com.zty.onlineedu.common.base.result.ResultCodeEnum;
+import com.zty.onlineedu.common.base.utils.ExceptionUtils;
+import com.zty.onlineedu.service.base.exceptions.GeneralException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+
 
 /**
  * @Author zty
@@ -16,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Api("文件服务")
 @CrossOrigin
+@Log4j2
 @RestController
 @RequestMapping("/service-files")
 public class FileDetailController {
@@ -29,9 +36,15 @@ public class FileDetailController {
     @PostMapping("/upload")
     public Result uploadFile(@ApiParam(value = "文件对象", required = true)
                              @RequestParam("file") MultipartFile file) {
-        FileInfo fileInfo = fileStorageService.of(file).upload();
+        try{
+            FileInfo fileInfo = fileStorageService.of(file).upload();
+            return fileInfo == null ? Result.ok().message("文件上传失败！") : Result.ok().data("fileInfo",fileInfo).message("文件上传成功");
 
-        return fileInfo == null ? Result.ok().message("文件上传失败！") : Result.ok().data("fileInfo",fileInfo).message("文件上传成功");
+        }catch (Exception e){
+            log.error(ExceptionUtils.getExceptionMessage(e));
+            throw new GeneralException(ResultCodeEnum.FILE_UPLOAD_ERROR);
+
+        }
 
 
     }
