7b4b7196e5c19de40212cc93a6034030b10a4cd3
ztyCode <1793959949@qq.com>
2023-03-25 16:11:39 +0800

    优化了日期时间工具类，通过传不同的日期时间格式，返回相对对应的的时间日期

--- common/common_util/src/main/java/com/zty/onlineedu/common/base/utils/LocalDateTimeUtils.java
+++ common/common_util/src/main/java/com/zty/onlineedu/common/base/utils/LocalDateTimeUtils.java
@@ -9,11 +9,28 @@ import java.time.format.DateTimeFormatter;
  * 时间日期工具类
  */
 public class LocalDateTimeUtils {
+    public static final String PATTERN_DATETIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String PATTERN_DATE_MINUTES = "yyyy-MM-dd HH:mm";
+    public static final String PATTERN_DATE_HOUR = "yyyy-MM-dd HH";
+    public static final String PATTERN_DATE = "yyyy-MM-dd";
+    public static final String PATTERN_OTHER_DATE = "yyyy/MM/dd";
+    public static final String PATTERN_YEAR_MONTH = "yyyy-MM";
+    public static final String PATTERN_TIME = "HH:mm:ss";
+
     public static String FormatNow(){
         LocalDateTime time = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String format = dtf.format(time);
         return format;
+    }
+
+
+    public static String customFormatDataTime(String formatStr){
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(formatStr);
+        String customFormatDataTime = dtf.format(time);
+        return customFormatDataTime;
+
     }
 
 
