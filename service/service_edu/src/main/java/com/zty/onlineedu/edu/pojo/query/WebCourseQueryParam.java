87c78d16bff09b9e71aac9795c1af6ab80f6245e
ztyCode <1793959949@qq.com>
2023-04-01 16:21:18 +0800

    创建前端服务器课程查询对象

---
+++ service/service_edu/src/main/java/com/zty/onlineedu/edu/pojo/query/WebCourseQueryParam.java
@@ -0,0 +1,31 @@
+package com.zty.onlineedu.edu.pojo.query;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author zty
+ * @Date 2023/4/1 16:13
+ * @deprecated 前端服务器 课程查询对象
+ */
+@Data
+@ApiModel("前端服务器课程查询对象")
+public class WebCourseQueryParam {
+    private static final long serialVersionUID =1L;
+
+    @ApiModelProperty(value="课程分类二级id")
+    private String subjectParentId;
+    @ApiModelProperty("课程分类一级id")
+    private String subjectId;
+
+    @ApiModelProperty(value = "销量")
+    private String buyCountSort;
+
+    @ApiModelProperty(value="最新")
+    private String gmtCreateSort;
+
+    @ApiModelProperty(value="价格")
+    private String priceSort;
+
+}
